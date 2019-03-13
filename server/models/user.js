c8d29c7471bfd02f350f47296186bab8571b3574
Mounika <vinnymounika96@gmail.com>
2019-03-13 11:14:08 -0400

    save changes

--- server/models/user.js
+++ server/models/user.js
@@ -2,32 +2,25 @@ const conn = require('./mysql_connection');
 
 const model = {
     getAll(cb){
-        conn.query("SELECT * FROM 2019Spring_Persons", (err, data) => {
+        conn.query("SELECT * FROM springinclass_2019", (err, data) => {
             cb(err, data);
         });    
     },
     get(id, cb){
-        conn.query("SELECT * From spring2019inclass_persons WHERE Id=?",id, (err,data) => {
-         cb(err,data[0]);
-        });
 
     },
     add(input, cb){
-        if(!input.password.length < 8){
-            cb(Error('A longer password is required'));
-            return;
+        if(!input.Person_Id){
+            cb(Error('Person_id is required'))
+            return
         }
-        conn.query( "INSERT INTO 2019Spring_Persons (FirstName,LastName,Birthday,Password,created_at) VALUES (?)",
-                    [[input.FirstName, input.LastName, input.Birthday, input.Password, new Date()]],
+        conn.query( "INSERT INTO springinclass_2019 (Person_Id,F_name,L_name) VALUES (?)",
+                    [[input.Person_Id, input.F_name, input.L_name]],
                     (err, data) => {
-                        if(err){
-                            cb(err);
-                            return;
-                        }
-                        model.get(data.insertId,(err,data)=>{
                         cb(err, data);
-                    })   
+                    }
+        );    
     }
 };
-
 module.exports = model;
+
